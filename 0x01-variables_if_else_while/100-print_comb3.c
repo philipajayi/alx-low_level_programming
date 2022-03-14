a5f0d6f42a9f089cbafc91aa8c3270af068547be
philipajayi <99433240+philipajayi@users.noreply.github.com>
2022-03-14 10:44:08 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
-
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: ALways 0 (Success)
+ */
 int main(void)
 {
-    
-	for (int i = 0; i <= 9; i++)
-	{
-	    for (int j = i + 1; j <= 9; j++)
-	    {
-	        putchar(i + '0');
-	        putchar(j + '0');
-	        if (((i * 10) + j) < 89)
-	        {
-	            putchar(',');
-                putchar(' ');
+	int n, m;
 
-	        }
-	    }
+	for (n = 48; n <= 56; n++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
 	}
 	putchar('\n');
-    return 0;
+	return (0);
 }
+© 2022 GitHub, Inc.
+Terms
+P
