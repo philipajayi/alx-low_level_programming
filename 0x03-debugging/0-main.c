da41fd09216a23560d183b3456e7943c85c39da5
philipajayi <99433240+philipajayi@users.noreply.github.com>
2022-03-20 16:12:08 +0100

    Update 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+void positive_or_negative(int i);
+/**
+* main - examines function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+int i;
+
+i = 2;
+positive_or_negative(i);
+
+return (0);
+}
