765076dd995980bc94c934737f14569e90d04e81
philipajayi <99433240+philipajayi@users.noreply.github.com>
2022-03-24 19:22:19 +0100

    Update 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+/**
+ * string_toupper - convert string to uppercase
+ * @str: string to convert
+ * Return: uppercase string.
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	}
+	return (str);
+
+}
